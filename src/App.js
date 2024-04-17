adc2b540df7d312a053ffcfdc5043e02ed50a780
IbragimSay <121868859+IbragimSay@users.noreply.github.com>
2024-04-17 09:43:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Map } from "./components";
+import "./App.css"
+import classNames from "classnames";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App () {
+  const [arr, setArr] = useState([
+    {id:1, title: "odin", activ: true},
+    {id: 2, title: "deti", activ: false},
+    {id: 3, title: "para", activ: false}
+  ])
+  const [activ, setActiv]= useState('')
+  const [filter, setFilter ] = useState(arr.filter(e=>{
+    if(e.activ)return e
+  }))
+
+
+  console.log(filter)
+
+  const tabe =(id)=>{
+    setArr(prive => 
+      prive.map(i=>
+      i.id === id
+      ? {...i, activ: true}
+      : {...i, activ: false}
+    ))
+  }
+  
+    return (
+      <div className="page"> {/* Контейнер с явно заданной высотой */}
+
+        <div className="filter">
+          {
+            arr.map(i=>(
+              <div key={i.id} onClick={()=>tabe(i.id)} className="box"><div className={classNames("activ", {
+                ["activ_not"]: i.activ
+              })}></div><p>{i.title}</p></div>
+            ))
+          }
+        </div>
+        <Map filValue={"para"}  />
+      </div>
+    );
+  
 }
-
-export default App;
