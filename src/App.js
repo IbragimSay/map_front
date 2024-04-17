6769c304cf42ad6ca257a6b8d1555af74d2d74ef
Sozdanieigry <sozdanieigry7777@gmail.com>
2024-04-17 14:07:12 +0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -3,43 +3,46 @@ import { Map } from "./components";
 import "./App.css"
 import classNames from "classnames";
 
-export default function App () {
+export default function App() {
   const [arr, setArr] = useState([
-    {id:1, title: "odin", activ: true},
-    {id: 2, title: "deti", activ: false},
-    {id: 3, title: "para", activ: false}
+    { id: 1, title: "one", activ: true },
+    { id: 2, title: "deti", activ: false },
+    { id: 3, title: "para", activ: false }
   ])
-  const [activ, setActiv]= useState('')
-  const [filter, setFilter ] = useState(arr.filter(e=>{
-    if(e.activ)return e
+  const [activ, setActiv] = useState('')
+  const [filter, setFilter] = useState(arr.filter(e => {
+    if (e.activ) return e
   }))
 
 
-  console.log(filter)
-
-  const tabe =(id)=>{
-    setArr(prive => 
-      prive.map(i=>
-      i.id === id
-      ? {...i, activ: true}
-      : {...i, activ: false}
-    ))
+  const tabe = (id) => {
+    setArr(prive =>
+      prive.map(i =>
+        i.id === id
+          ? { ...i, activ: true }
+          : { ...i, activ: false }
+      ))
   }
-  
-    return (
-      <div className="page"> {/* Контейнер с явно заданной высотой */}
+  let [fil, setFil] = useState('para')
 
-        <div className="filter">
-          {
-            arr.map(i=>(
-              <div key={i.id} onClick={()=>tabe(i.id)} className="box"><div className={classNames("activ", {
-                ["activ_not"]: i.activ
-              })}></div><p>{i.title}</p></div>
-            ))
-          }
-        </div>
-        <Map filValue={"para"}  />
+
+  return (
+    <div className="page"> {/* Контейнер с явно заданной высотой */}
+
+      <div className="filter">
+        {
+          arr.map(i => (
+            <div key={i.id} onClick={() => {
+              tabe(i.id)
+              setFil(i.title)
+            }} className="box"><div className={classNames("activ", {
+              ["activ_not"]: i.activ
+            })}></div><p>{i.title}</p></div>
+          ))
+        }
       </div>
-    );
-  
+      <Map filValue={fil} />
+    </div>
+  );
+
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,90 +1,73 @@
-import React, {useState, useEffect} from 'react'
-import {MapContainer, TileLayer, Marker, useMapEvents, Popup, Polyline, useMap, } from 'react-leaflet'
+import React, { useState, useEffect } from 'react'
+import { MapContainer, TileLayer, Marker, useMapEvents, Popup, Polyline, useMap, } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'; // Убедитесь, что подключен CSS Leaflet
 import './map.css'
 import L, { Icon, map, point, polyline } from 'leaflet'
 import axios from 'axios';
-console.log('argen')
 
-export function Map({filValue}) {
+
+export function Map({ filValue }) {
   const [line, setLine] = useState([])
-    
   const [pointer, setPointer] = useState([
-    {id: 2, position: [37.631857,55.829856], title: "fontan" , teg: "deti" },
-    {id: 2, position: [37.625171,55.833239], title: "cheburek" , teg: "one" },
-    {id: 6, position: [37.634592,55.833356], title: "dom cultur" , teg: "para" },
-      {id: 5, position: [37.638585, 55.830391], title: "moscov ekspa" , teg: "deti" },
-      {id: 4, position: [ 37.637522,55.829654], title: "aly parusa" , teg: "para" },
-        {id: 3, position:[37.637777,55.828552], title: "mac" , teg: "one" },
-        
-    ])
+    { id: 2, position: [37.631857, 55.829856], time: 20, title: "fontan", teg: "deti" },
+    { id: 9, position: [37.628180, 55.830380], time: 20, title: "cheburek", teg: "one" },
+    { id: 2, position: [37.625171, 55.833239], time: 20, title: "cheburek", teg: "one" },
+    { id: 6, position: [37.634592, 55.833356], time: 20, title: "dom cultur", teg: "para" },
+    { id: 5, position: [37.638585, 55.830391], time: 20, title: "moscov ekspa", teg: "deti" },
+    { id: 4, position: [37.637522, 55.829654], time: 20, title: "aly parusa", teg: "para" },
+    { id: 3, position: [37.637777, 55.828552], time: 20, title: "mac", teg: "one" },
 
-    const fil = filValue
-    const dataC = pointer.filter(i=>i.teg == fil)
+  ])
+  const [loading, setLoading] = useState(false)
 
+  const fil = filValue
+  let dataC = []
 
-    const LeafIcon = L.Icon.extend({
-        options: {
-            iconSize: [38, 38]
-        }
-    })
-    
-    const [mapC, setMapC] = useState([])
-    const getPointer =  async ( index)=>{
-      
-      console.log(dataC)
-       dataC.forEach(i=>{
-          setMapC(mapC.push(i.position.reverse()))
-       })
-      console.log(mapC)
-      const cordinat = await  axios.post(`https://graphhopper.com/api/1/route?key=cf044edf-53fa-411f-8b5d-80c0f2ff4875`, {
-        points: mapC,
-        snap_preventions: ['motorway', 'ferry', 'tunnel'],
-        details: ['road_class', 'surface'],
-        profile: 'foot',
-        locale: 'en',
-        instructions: true,
-        calc_points: true,
-        points_encoded: false,
-      
-      })
-      
-      const arr2 = cordinat.data.paths[0].points.coordinates.map(i=>{
-        
-        return i.reverse()
-      })
-      setLine(arr2)
-      
+  const LeafIcon = L.Icon.extend({
+    options: {
+      iconSize: [38, 38]
     }
-    useEffect(()=>{
-      getPointer()
-      
-    },[fil])
+  })
 
-    
+  const getPointer = async () => {
+    dataC = pointer.filter(i => i.teg == fil)
+    let argen = []
+    dataC.forEach(i => {
+      argen.push(i.position)
+    })
 
+    const cordinat = await axios.post(`https://graphhopper.com/api/1/route?key=cf044edf-53fa-411f-8b5d-80c0f2ff4875`, {
+      points: argen,
+      snap_preventions: ['motorway', 'ferry', 'tunnel'],
+      details: ['road_class', 'surface'],
+      profile: 'foot',
+      locale: 'en',
+      instructions: true,
+      calc_points: true,
+      points_encoded: false,
 
-    const leafIcon = new LeafIcon({iconUrl: "https://cdn-icons-png.flaticon.com/512/1201/1201643.png"});
-    const mestoIcon = new LeafIcon({iconUrl: "https://cdn-icons-png.flaticon.com/128/9931/9931684.png"});
+    })
+    const arr2 = cordinat.data.paths[0].points.coordinates.map(i => {
+      return i.reverse()
+    })
+    setLine(arr2)
+  }
+  useEffect(() => {
+    setLoading(false)
+    getPointer()
+    setLoading(true)
+  }, [fil])
+
+  const leafIcon = new LeafIcon({ iconUrl: "https://cdn-icons-png.flaticon.com/512/1201/1201643.png" });
+  const mestoIcon = new LeafIcon({ iconUrl: "https://cdn-icons-png.flaticon.com/128/9931/9931684.png" });
   return (
-        <MapContainer className='map' center={[55.826197, 37.637720]} zoom={15}>
-          <Polyline positions={line} color='red' />
-        <TileLayer
-            attribution='<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[55.826197, 37.637720]} icon={leafIcon}>
-          </Marker>
-          {
-            dataC.map(i=>(
-                <Marker key={i.id} position={i.position.reverse()} icon={mestoIcon}>
-                  <Popup>{i.teg}</Popup>
-                </Marker>
-            ))
-          }
-        
-        </MapContainer>
+    <MapContainer className='map' center={[55.826197, 37.637720]} zoom={14}>
+      <Polyline positions={line} color='red' />
+      <TileLayer
+        attribution='<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
 
   )
 }
-  
