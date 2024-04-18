60d49e4ea5612911832aae6ae523475ff3b1a35a
Sozdanieigry <sozdanieigry7777@gmail.com>
2024-04-18 11:44:53 +0300

    commit 5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ export default function App() {
   const [arr, setArr] = useState([
     { id: 1, text: "Один", title: "one", activ: true },
     { id: 2, text: "Семья", title: "deti", activ: false },
-    { id: 3, text: "Пара", title: "para", activ: false }
+    { id: 3, text: "Пара", title: "para", activ: false },
+    { id: 4, text: "Друзя", title: "druzy", activ: false }
   ])
   const [activ, setActiv] = useState('')
   const [filter, setFilter] = useState(arr.filter(e => {
@@ -32,21 +33,21 @@ export default function App() {
       <div className="filter">
         <h2 className="filter__title">Категории</h2>
         <div className="filter__block">
-        {
-          arr.map(i => (
-            <div className={classNames("box", {
-              ["box__activ"]: i.activ
-            })} key={i.id} onClick={() => {
-              tabe(i.id)
-              setFil(i.title)
-            }}>{i.text}</div>
-          ))
-        }
+          {
+            arr.map(i => (
+              <div className={classNames("box", {
+                ["box__activ"]: i.activ
+              })} key={i.id} onClick={() => {
+                tabe(i.id)
+                setFil(i.title)
+              }}>{i.text}</div>
+            ))
+          }
         </div>
       </div>
-      
+
       <Map filValue={fil} />
-      
+
     </div>
   );
 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -9,41 +9,49 @@ import axios from 'axios';
 export function Map({ filValue }) {
   const [line, setLine] = useState([])
   const [pointer, setPointer] = useState([
-    { id: 2, position: [37.639728,55.822728], img: "", time: 40, title: "Музей космонавтики", teg: "deti" },
+    { id: 2, position: [37.639728, 55.822728], img: "", time: 40, title: "Музей космонавтики", teg: "deti" },
     { id: 2, position: [37.631857, 55.829856], img: "", time: 20, title: "Фонтан Дружба народов", teg: "deti" },
-    { id: 5, position: [37.638585, 55.830391], img: "", time: 20, title: "ВДНХ Экспо",  teg: "deti" },
-    { id: 2, position: [37.625171, 55.833239], img: "", time: 20, title: "cheburek", teg: "one" },
-    { id: 9, position: [37.628180, 55.830380],img: "", time: 20, title: "cheburek", teg: "one" },
-    { id: 3, position: [37.637777, 55.828552],img: "", time: 20, title: "mac", teg: "one" },
-    { id: 6, position: [37.618359,55.832956],img: "", time: 20, title: "Москвариум", teg: "para" },
-    { id: 6, position: [37.634592, 55.833356],img: "", time: 20, title: "Дом культуры", teg: "para" },
-    { id: 4, position: [37.637522, 55.829654],img: "", time: 20, title: "Алые паруса", teg: "para" },
-    { id: 4, position: [37.637633,55.826303],img: "", time: 20, title: "aly parusa", teg: "deti" },
-    { id: 4, position: [37.637633,55.826303],img: "", time: 20, title: "aly parusa", teg: "one" },
-    { id: 4, position: [37.637633,55.826303],img: "", time: 20, title: "aly parusa", teg: "para" },
+    { id: 5, position: [37.638585, 55.830391], img: "", time: 20, title: "ВДНХ Экспо", teg: "deti" },
+    { id: 2, position: [37.620173, 55.836090], img: "", time: 20, title: "Павильон № 34 Российский космос: создавая будущее", teg: "one" },
+    { id: 9, position: [37.629901, 55.835087], img: "https://avatars.mds.yandex.net/get-altay/13091609/2a0000018e2e4fb4bc2aaf869219835814aa/XXXL", time: 20, title: "Совиный дом", teg: "one" },
+    { id: 3, position: [37.633566, 55.828773], img: "", time: 20, title: "Павильон № 1 Первые в России – Стране возможностей!", teg: "one" },
+    { id: 6, position: [37.618359, 55.832956], img: "", time: 20, title: "Москвариум", teg: "para" },
+    { id: 6, position: [37.634592, 55.833356], img: "", time: 20, title: "Дом культуры", teg: "para" },
+    { id: 4, position: [37.637522, 55.829654], img: "", time: 20, title: "Алые паруса", teg: "para" },
+    { id: 13, position: [37.616377, 55.838392], img: "", title: "Третий Каменский пруд", teg: "druzy" },
+    { id: 12, position: [37.616686, 55.834452], img: "", title: "Мичуринский сад", teg: "druzy" },
+    { id: 11, position: [37.623444, 55.835471], img: "", time: 20, title: "Смайл Парк", teg: "druzy" },
+
+    { id: 4, position: [37.637633, 55.826303], img: "", time: 20, title: "aly parusa", teg: "deti" },
+    { id: 4, position: [37.637633, 55.826303], img: "", time: 20, title: "aly parusa", teg: "one" },
+    { id: 4, position: [37.637633, 55.826303], img: "", time: 20, title: "aly parusa", teg: "para" },
+    { id: 4, position: [37.637633, 55.826303], img: "", time: 20, title: "aly parusa", teg: "druzy" },
   ])
   const [pointClone, setPointerClone] = useState([
 
-      { id: 2, position: [55.822728, 37.639728], img: "https://cdn.culture.ru/images/4d639fcf-7787-5992-89b3-2b682a74eeb0", time: 20, title: "Музей космонавтики", teg: "deti" },
-      { id: 2, position: [55.829856,37.631857], img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/31/1e/39/the-fountain.jpg?w=1100&h=-1&s=1",  time: 20, title: "Фонтан Дружба народов", teg: "deti" },
-      { id: 5, position: [55.830391,37.638585], img: "https://regions.kidsreview.ru/sites/default/files/styles/oww/public/12/12/2014_-_1746/vdnh-ekspo.jpg", time: 20, title: "ВДНХ Экспо", teg: "deti" },
-      { id: 2, position: [55.833239, 37.625171], img: "", time: 20, title: "cheburek", teg: "one" },
-      { id: 9, position: [55.830380,37.628180],img: "", time: 20, title: "cheburek", teg: "one" },
-      { id: 3, position: [55.828552,37.637777],img: "", time: 20, title: "mac", teg: "one" },
-      { id: 6, position: [55.833322, 37.618515],img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/6e/ad/27/img-20180620-104558-largejpg.jpg?w=1400&h=-1&s=1", time: 20, title: "Москвариум", teg: "para" },
-      { id: 6, position: [55.833356,37.634592],img: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Moscow%2C_VDNKh%2C_model_House_of_Culture_%2810656153396%29.jpg", time: 20, title: "Дом культуры", teg: "para" },
-      { id: 4, position: [55.829654,37.637522],img: "https://cdn.spbdnevnik.ru/uploads/block/image/836838/__medium_%D0%B0%D0%BB%D1%8B%D0%B5%D0%BF%D0%B0%D1%80%D1%83%D1%81%D0%B0%D1%84%D1%83%D1%84%D0%B0%D0%B5%D0%B2.jpg.jpg", time: 20, title: "Алые паруса", teg: "para" },
-      { id: 10, position: [55.826303,37.637633],img: "https://um.mos.ru/uploads/house/media/4076/16392ccfaed501.jpg", time: 20, title: "Главный вход ВДНХ", teg: "deti" },
-      { id: 10, position: [55.826303,37.637633], img: "https://um.mos.ru/uploads/house/media/4076/16392ccfaed501.jpg", time: 20, title: "Главный вход ВДНХ", teg: "one" },
-      { id: 10, position: [55.826303,37.637633],img: "https://um.mos.ru/uploads/house/media/4076/16392ccfaed501.jpg", time: 20, title: "Главный вход ВДНХ", teg: "para" },
-    
+    { id: 2, position: [55.822728, 37.639728], img: "https://cdn.culture.ru/images/4d639fcf-7787-5992-89b3-2b682a74eeb0", time: 20, title: "Музей космонавтики", teg: "deti" },
+    { id: 2, position: [55.829856, 37.631857], img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/31/1e/39/the-fountain.jpg?w=1100&h=-1&s=1", time: 20, title: "Фонтан Дружба народов", teg: "deti" },
+    { id: 5, position: [55.830391, 37.638585], img: "https://regions.kidsreview.ru/sites/default/files/styles/oww/public/12/12/2014_-_1746/vdnh-ekspo.jpg", time: 20, title: "ВДНХ Экспо", teg: "deti" },
+    { id: 2, position: [55.836090, 37.620173], img: "https://core-pht-proxy.maps.yandex.ru/v1/photos/download?photo_id=JbZ4Sv9YCan8DZnSUsm&image_size=X5L", time: 20, title: "Павильон № 34 Российский космос: создавая будущее", teg: "one" },
+    { id: 9, position: [55.835087, 37.629901], img: "https://avatars.mds.yandex.net/get-altay/13091609/2a0000018e2e4fb4bc2aaf869219835814aa/XXXL", time: 20, title: "Совиный дом", teg: "one" },
+    { id: 3, position: [55.828773, 37.633566], img: "https://img.29palms.ru/photo/hotels/russia/moscow/vdnkh-main-pavilion/resized/016_Rossiya_Moskva_Central'naya_alleya_i_Glavnyy_pavil'on_VDNH_The_main_pavilion_in_VDNKh_Moscow_Russia_Foto_koromelenayandexru_-_Depositphotos.jpg", time: 20, title: "Павильон № 1 Первые в России – Стране возможностей!", teg: "one" },
+    { id: 6, position: [55.833322, 37.618515], img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/6e/ad/27/img-20180620-104558-largejpg.jpg?w=1400&h=-1&s=1", time: 20, title: "Москвариум", teg: "para" },
+    { id: 6, position: [55.833356, 37.634592], img: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Moscow%2C_VDNKh%2C_model_House_of_Culture_%2810656153396%29.jpg", time: 20, title: "Дом культуры", teg: "para" },
+    { id: 4, position: [55.829654, 37.637522], img: "https://cdn.spbdnevnik.ru/uploads/block/image/836838/__medium_%D0%B0%D0%BB%D1%8B%D0%B5%D0%BF%D0%B0%D1%80%D1%83%D1%81%D0%B0%D1%84%D1%83%D1%84%D0%B0%D0%B5%D0%B2.jpg.jpg", time: 20, title: "Алые паруса", teg: "para" },
+    { id: 11, position: [55.835471, 37.623444], title: "Смайл Парк", img: "https://avatars.mds.yandex.net/get-altay/2436798/2a0000018201e0723a389b9f93c5e86c790b/XXXL", teg: "druzy" },
+    { id: 12, position: [55.834452, 37.616686], title: "Мичуринский сад", img: "https://core-pht-proxy.maps.yandex.ru/v1/photos/download?photo_id=KJVNH50QxmAdcW7DYZbZ3g&image_size=XXXL", teg: "druzy" },
+    { id: 13, position: [55.838392, 37.616377], img: "https://yamoscow.ru/wp-content/uploads/2021/08/Пруд-с-фонтаном-Золотой-колос-на-ВДНХ-1080x720.jpg", title: "Третий Каменский пруд", teg: "druzy" },
+    { id: 10, position: [55.826303, 37.637633], img: "https://um.mos.ru/uploads/house/media/4076/16392ccfaed501.jpg", time: 20, title: "Главный вход ВДНХ", teg: "deti" },
+    { id: 10, position: [55.826303, 37.637633], img: "https://um.mos.ru/uploads/house/media/4076/16392ccfaed501.jpg", time: 20, title: "Главный вход ВДНХ", teg: "one" },
+    { id: 10, position: [55.826303, 37.637633], img: "https://um.mos.ru/uploads/house/media/4076/16392ccfaed501.jpg", time: 20, title: "Главный вход ВДНХ", teg: "para" },
+    { id: 10, position: [55.826303, 37.637633], img: "https://um.mos.ru/uploads/house/media/4076/16392ccfaed501.jpg", time: 20, title: "Главный вход ВДНХ", teg: "druzy" },
   ])
   const [time, setTime] = useState(0)
 
 
 
-  
-  
+
+
 
   const fil = filValue
   let dataC = []
@@ -55,8 +63,8 @@ export function Map({ filValue }) {
 
   let ar
   ar = pointClone.filter(i => i.teg == fil)
-  
-  
+
+
   const getPointer = async () => {
     dataC = pointer.filter(i => i.teg == fil)
     let time2 = 0
@@ -91,14 +99,14 @@ export function Map({ filValue }) {
   const mestoIcon = new LeafIcon({ iconUrl: "image/point.svg" });
   return (
     <div className='contener'>
-      <MapContainer  className='map' center={[55.831605, 37.629239]} zoom={15}>
-        
+      <MapContainer className='map' center={[55.831605, 37.629239]} zoom={15}>
+
         <TileLayer
           attribution='<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {
-          ar.map(i=>(
+          ar.map(i => (
             <Marker icon={mestoIcon} position={i.position}>
               <Popup className='custom_popup'>
                 <img className='image' src={i.img} alt="" />
@@ -107,9 +115,9 @@ export function Map({ filValue }) {
             </Marker>
           ))
         }
-        
 
-        <Polyline dashArray={[5, 5]}  positions={line} color='#000' />
+
+        <Polyline dashArray={[5, 5]} positions={line} color='#000' />
       </MapContainer>
       <div className='contener__time'>Средняя продолжительность маршрута: <span> {time} M</span></div>
     </div>
